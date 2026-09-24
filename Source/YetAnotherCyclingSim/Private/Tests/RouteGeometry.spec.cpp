758700987dbd597c4842484de0bdaa1ebfa5757e
karnalooch <1234490+karnalooch@users.noreply.github.com>
2026-09-24 17:10:09 +0200

    chore(stage3): make geometry test limits dependency explicit

--- Source/YetAnotherCyclingSim/Private/Tests/RouteGeometry.spec.cpp
+++ Source/YetAnotherCyclingSim/Private/Tests/RouteGeometry.spec.cpp
@@ -1,6 +1,7 @@
 #if WITH_DEV_AUTOMATION_TESTS
 
 #include "Misc/AutomationTest.h"
+#include "Math/NumericLimits.h"
 
 #include "Cycling/AlpineJourneyGeometry.h"
 #include "Cycling/AlpineJourneyRoute.h"
